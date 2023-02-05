189306a5279f54ebd2b89f0e58c53d217a43a9e4
siwhelan <74879900+siwhelan@users.noreply.github.com>
2023-02-05 18:01:44 +0000

    Updated several functions, added scripts.py

--- recipes.py
+++ recipes.py
@@ -1,20 +1,22 @@
 import pymongo
-from ingredients import ingredients
+import ingredients
+from ingredients import new_ingredient
+
+
+
 
 def connect_to_db():
     # Connect to the MongoDB server
     client = pymongo.MongoClient("mongodb://localhost:27017/")
-    
+
     # Get the recipes database
     db = client["stock_control"]
-    
+
     # Get the recipes collection
     recipes_collection = db["recipes"]
     ingredients_collection = db["ingredients"]
 
-    
-    
-    return recipes_collection
+    return recipes_collection, ingredients_collection
 
 
 # Define a class that takes name and ingredients and produces the cost of the recipe
@@ -23,17 +25,6 @@ class Recipe:
     def __init__(self, name, ingredients):
         self.name = name
         self.ingredients = ingredients
-
-    # Calculate the total cost of the recipe by summing up the cost per gram of each ingredient
-    def total_cost(self):
-        total = 0
-        for ingredient, amount in self.ingredients.items():
-            # Find the Item object in the ingredients list that corresponds to this ingredient
-            item = next((x for x in ingredients if x.ingredient == ingredient), None)
-            if item:
-                # Calculate the cost of this ingredient by multiplying its cost per gram by the amount in grams
-                total += item.price_per_gram() * amount
-        return total
 
     # Define a string representation of the Recipe object
     def __str__(self):
@@ -46,5 +37,53 @@ class Recipe:
         )
         return f"{self.name}: {ingredients_str}"
 
-    def add_recipe(self):
-        pass
+    def add_recipe():
+        # Connect to the recipes collection
+        recipes_collection, ingredients_collection = connect_to_db()
+
+        # Get the code of the last recipe in the database
+        last_recipe = recipes_collection.find().sort([("code", -1)]).limit(1)
+
+        if last_recipe is not None:
+            # Get the code of the last recipe in the database
+            last_recipe = last_recipe[0]
+            code = int(last_recipe["code"]) + 1
+        else:
+            code = 1
+
+        # Format the code to a 5-digit string (e.g. 00001, 00002, etc.)
+        recipe_code = str(code).zfill(5)
+
+        # Ask user for recipe name
+        recipe_name = input("Enter recipe name: ")
+        # Create a dictionary to store ingredients and their amounts
+        ingredients = {}
+        while True:
+            ingredient = input("Enter ingredient name (or enter 'done' when finished): ")
+            if ingredient == 'done':
+                break
+            # Check if the ingredient exists in the ingredients collection
+            existing_product = ingredients_collection.find_one({"ingredient": ingredient})
+            if existing_product:
+                # Check if the ingredient amount is a valid integer
+                try:
+                    amount = int(input(f"Enter amount for {ingredient}: "))
+                    ingredients[ingredient] = amount
+                except ValueError:
+                    print(f"Amount for {ingredient} must be a number. Please try again.")
+            else:
+                print(f"Ingredient {ingredient} does not exist. Please add it to the database first.")
+                # Call the new_ingredient function to add the ingredient to the database
+                new_ingredient()
+                # Check if the ingredient amount is a valid integer
+                try:
+                    amount = int(input(f"Enter amount of {ingredient} required for the recipe: "))
+                    ingredients[ingredient] = amount
+                except ValueError:
+                    print(f"Amount for {ingredient} must be a number. Please try again.")
+        # Insert the recipe into the recipes collection
+        recipe = {"name": recipe_name, "ingredients": ingredients, 'code': recipe_code}
+        recipes_collection.insert_one(recipe)
+        print(f"Successfully added {recipe_name} recipe to the database!")
+
+
---
+++ scripts.py
@@ -0,0 +1,72 @@
+# Notebook style file for random scripts needed throught out development #
+
+# Update all recipes with a new field asssigning them a code
+
+def add_recipe_codes():
+
+    from pymongo import MongoClient
+
+    # Connect to the database
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["stock_control"]
+    recipes_collection = db["recipes"]
+
+    # Get the current number of recipes
+    recipe_count = recipes_collection.count_documents({})
+
+    # Update the code for each recipe in the collection
+    for i in range(recipe_count):
+        # Get the current recipe
+        recipe = recipes_collection.find()[i]
+
+        # Generate the new 5-digit code
+        new_code = str(i + 1).zfill(5)
+
+        # Update the recipe with the new code
+        recipes_collection.update_one({"_id": recipe["_id"]}, {
+                                      "$set": {"code": new_code}})
+
+    print("All recipes have been updated with new codes.")
+
+add_recipe_codes()
+
+# Calculate the total cost of the recipe by summing up the cost per gram of each ingredient
+
+def calculate_recipe_costs(recipe, ingredients_collection):
+
+    cost = 0
+
+    # Loop through each ingredient in the recipe
+    for ingredient, quantity in recipe["ingredients"].items():
+        # Retrieve the information for the current ingredient from the ingredients collection
+        ingredient_info = ingredients_collection.find_one({"ingredient": ingredient})
+        # Calculate the cost of the ingredient by multiplying the quantity needed by the price per pack
+        ingredient_cost = quantity * ingredient_info["price_per_pack"] / ingredient_info["pack_size"]
+        # Add the cost of the ingredient to the total cost
+        cost += ingredient_cost
+
+    # Update the recipe document with the calculated cost
+    recipe["recipe_cost"] = cost
+    return recipe
+
+if __name__ == "__main__":
+    from pymongo import MongoClient
+    # Connect to the MongoDB client
+    client = MongoClient("mongodb://localhost:27017/")
+    # Select the "cookbook" database
+    db = client["stock_control"]
+    # Select the "recipes" collection
+    recipes_collection = db["recipes"]
+    # Select the "ingredients" collection
+    ingredients_collection = db["ingredients"]
+    # Find all recipe documents in the database
+    recipes = recipes_collection.find()
+    # Loop through each recipe
+    for recipe in recipes:
+        # Calculate the cost of the recipe
+        recipe = calculate_recipe_costs(recipe, ingredients_collection)
+        # Update the recipe document in the recipes collection with the calculated cost
+        recipes_collection.update_one({"_id": recipe["_id"]}, {"$set": {"recipe_cost": recipe["recipe_cost"]}})
+        # Print a success message
+        print("Successfully calculated recipe costs!")
+
