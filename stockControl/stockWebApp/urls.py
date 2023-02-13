3b71c988012fc4b2cfde04eec001dae4c8d5bf0e
siwhelan <74879900+siwhelan@users.noreply.github.com>
2023-02-13 16:41:52 +0000

    More general progress - added stock input page

--- stockControl/stockWebApp/urls.py
+++ stockControl/stockWebApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    #path('', views.index),
     path('', views.stock_control, name='stock_control'),
+    path('stocktake', views.stocktake, name='stocktake')
 ]
--- stockControl/stockWebApp/views.py
+++ stockControl/stockWebApp/views.py
@@ -20,7 +20,8 @@ def stock_control(request):
     for ingredient in ingredients:
         ingredient['ingredient'] = ingredient['ingredient'].title()
         # Round each price to 2 decimal places
-        ingredient['price_per_pack'] = "{:.2f}".format(ingredient['price_per_pack'])
+        ingredient['price_per_pack'] = "{:.2f}".format(
+            ingredient['price_per_pack'])
         pack_size = ingredient['pack_size']
         # Round each pack size to 2 decimal places if float
         if int(pack_size) == pack_size:
@@ -31,7 +32,38 @@ def stock_control(request):
         # Append to the capitalized_ingredients list
         capitalized_ingredients.append(ingredient)
 
-
     # Render the "index.html" template with the capitalized ingredients
     return render(request, 'index.html', {'ingredients': capitalized_ingredients})
 
+
+def stocktake(request):
+
+    # Connect to the MongoDB database
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client["stock_control"]
+    ingredients_collection = db["ingredients"]
+
+    # Retrieve all ingredients from the "ingredients" collection
+    ingredients = ingredients_collection.find()
+
+    # Initialize an empty list to store the capitalized ingredients
+    capitalized_ingredients = []
+
+    # Loop through each ingredient and capitalize each word in the "ingredient" field
+    for ingredient in ingredients:
+        ingredient['ingredient'] = ingredient['ingredient'].title()
+        # Round each price to 2 decimal places
+        ingredient['price_per_pack'] = "{:.2f}".format(
+            ingredient['price_per_pack'])
+        pack_size = ingredient['pack_size']
+        # Round each pack size to 2 decimal places if float
+        if int(pack_size) == pack_size:
+            pack_size = "{:.0f}".format(pack_size)
+        else:
+            pack_size = "{:.2f}".format(pack_size)
+        ingredient['pack_size'] = pack_size
+        # Append to the capitalized_ingredients list
+        capitalized_ingredients.append(ingredient)
+
+    # Render the "index.html" template with the capitalized ingredients
+    return render(request, 'stocktake.html', {'ingredients': capitalized_ingredients})
