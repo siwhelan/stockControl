46de0d75a58e8036a7fa5fbeb6a544cdce43b082
siwhelan <74879900+siwhelan@users.noreply.github.com>
2023-02-14 21:02:12 +0000

    File alterations

--- stockControl/stockWebApp/urls.py
+++ stockControl/stockWebApp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.stock_control, name='stock_control'),
-    path('stocktake', views.stocktake, name='stocktake')
+    path('', views.home, name='home'),
+    path('ingredients/view/', views.view_ingredients, name='view_ingredients'),
+    path('stocktake', views.stocktake, name='stocktake'),
+    path('viewrecipe', views.view_recipe, name="view_recipe"),
 ]
--- stockControl/stockWebApp/views.py
+++ stockControl/stockWebApp/views.py
@@ -1,10 +1,11 @@
+from pymongo import MongoClient
 from django.shortcuts import render
 import pymongo
 
 # Create your views here.
 
 
-def stock_control(request):
+def view_ingredients(request):
     # Connect to the MongoDB database
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     db = client["stock_control"]
@@ -33,7 +34,7 @@ def stock_control(request):
         capitalized_ingredients.append(ingredient)
 
     # Render the "index.html" template with the capitalized ingredients
-    return render(request, 'index.html', {'ingredients': capitalized_ingredients})
+    return render(request, 'view_all_ing.html', {'ingredients': capitalized_ingredients})
 
 
 def stocktake(request):
@@ -67,3 +68,34 @@ def stocktake(request):
 
     # Render the "index.html" template with the capitalized ingredients
     return render(request, 'stocktake.html', {'ingredients': capitalized_ingredients})
+
+# View Home Page (currently navbar only)
+
+
+def home(request):
+    return render(request, 'base.html')
+
+
+def view_recipe(request):
+    # Connect to MongoDB
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['stock_control']
+    recipes_collection = db['recipes']
+
+    # Get all recipes from MongoDB
+    recipes = recipes_collection.find()
+
+    # Create list of recipe data for rendering in template
+    recipe_data = []
+    for recipe in recipes:
+        recipe_data.append({
+            'name': recipe['name'],
+            'ingredients': recipe['ingredients'],
+            'code': recipe['code'],
+            'recipe_cost': recipe['recipe_cost']
+        })
+
+    context = {
+        'recipe_data': recipe_data
+    }
+    return render(request, 'recipe_card.html', context)
